ae7cf6a66efb412d35abef3d2e2e88316b57ff05
Artistarasou <162071814+Artistarasou@users.noreply.github.com>
2024-05-22 23:29:04 +0700

    Create ThirdGoal.js

---
+++ ThirdGoal.js
@@ -0,0 +1,13 @@
+// src/components/ThirdGoal.js
+import React from 'react';
+
+function ThirdGoal() {
+  return (
+    <div>
+      <h2>Third Goal</h2>
+      <p>update linkedin</p>
+    </div>
+  );
+}
+
+export default ThirdGoal;
